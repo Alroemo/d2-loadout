3093a0c3c2c0774c764bccd239974529b2ebc22e
Alex Roeum <alex@Alexs-MacBook-Pro.local>
2021-10-31 11:58:12 -0400

    Git init

---
+++ README.md
@@ -0,0 +1,25 @@
+# destiny-profiles
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+# d2-loadout
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <loadout></loadout>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import loadout from './components/loadout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    loadout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
